b05d559a30b40b0f90cec6513909b08957c97b82
cyril-pallasigui <46484675+cyril-pallasigui@users.noreply.github.com>
2019-01-11 20:30:01 +0800

    upload Background Generator

---
+++ script.js
@@ -0,0 +1,46 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomize = document.querySelector("#randomize");
+
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ " , "
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomizeColors() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randomize.addEventListener("click", randomizeColors);
+
+body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ " , "
+	+ color2.value
+	+ ")";
+
+css.textContent = body.style.background + ";";
